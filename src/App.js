3a117c418f123db42937ba512cfe92d39fd64551
Muhammed_El_Fandary <melfandary@gmail.com>
2023-01-05 19:00:14 +0200

    Created basic pages and routing layout.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from './pages/Login';
+import Register from "./pages/Register";
+import Singleblog from "./pages/Singleblog";
+import Write from "./pages/Write";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const PageLayout = () => {
+  return(
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PageLayout />,
+    children:[
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/post/:id',
+        element: <Singleblog />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+      
+    ],
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+      <RouterProvider router={router} />
+
+      </div>
+    
     </div>
   );
 }
